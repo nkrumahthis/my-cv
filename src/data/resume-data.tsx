2a6cb52a414e423791e82247cabb5d46811739b8
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-01-07 15:14:35 +0000

    recount yoe

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -27,9 +27,9 @@ export const RESUME_DATA = {
   location: "Accra, Ghana, GMT",
   locationLink: "https://www.google.com/maps/place/Accra",
   about:
-    "Software Engineer, 7yoe. Programming polyglot. Fintech, open-source, automation ninja",
+    "Software Engineer, 6yoe. Programming polyglot. Fintech, open-source, automation ninja",
   summary:
-    "Backend Engineer with 7+ years, building robust systems for growing companies. 2023 eCedi Hackathon finalist. Upgraded a crucial fintech server that handles significant throughput of transactions (now over $93 million daily on average and doubling each year). Maintainer of 3 Open Source packages. Focused on creating tools and automations for smooth development and maintenance amidst ongoing growth. Always eager to try new technologies. A programming language polyglot.",
+    "Backend Engineer with 6+ years, building robust systems for growing companies. 2023 eCedi Hackathon finalist. Upgraded a crucial fintech server that handles significant throughput of transactions (now over $93 million daily on average and doubling each year). Maintainer of 3 Open Source packages. Focused on creating tools and automations for smooth development and maintenance amidst ongoing growth. Always eager to try new technologies. A programming language polyglot.",
   avatarUrl: "https://avatars.githubusercontent.com/u/42615665?v=4",
   personalWebsiteUrl: "https://nkrumahsarpong.com",
   blogUrl: "https://dev.to/nkrumahthis",
